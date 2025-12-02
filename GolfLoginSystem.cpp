e14f5ea0ef14f393272a2d433ee7b2a845f5f549
Daniel167125 <c24410814@myTUDublin.ie>
2025-12-02 12:52:43 +0000

    Add files via upload

--- GolfLoginSystem.cpp
+++ GolfLoginSystem.cpp
@@ -1,3 +1,6 @@
+/// Daniel O'Hare C24410814 - Golf Login System CPP File - Programming Project assessment 
+
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -8,8 +11,8 @@ using namespace std;
 
 class UserLogin {
 private:
-    string username;
-    string password;
+    string username;  
+    string password;   // private strings to hold usernames and passwords of members
 
 public:
     UserLogin( string user, string pass) : username(user), password(pass) {}
@@ -19,7 +22,7 @@ public:
     }
     string getPassword() const { 
         return password; 
-    }
+    } // getter functions to obtain username and password
 };
 
 // class to allow manager to login to scorecard system
@@ -27,7 +30,7 @@ public:
 class ManagerLogin {
 private:
     string username;
-    string password;
+    string password;  // private strings to hold usernames and passwords of managers
 
 public:
     UserLogin(string user,string pass) : username(user), password(pass) {}
@@ -36,7 +39,7 @@ public:
         return username; 
     }
     string getPassword() const { 
-        return password; 
+        return password;  // getter functions to obtain username and password
     }
 };
 
@@ -44,7 +47,7 @@ public:
 
 class LoginSystem {
 private:
-    vector<UserLogin> users; // user logins
+    vector<UserLogin> users; // uer logins
     vector<ManagerLogin> managers; // manger logins
 
 public:
@@ -65,7 +68,7 @@ LoginManager() {
 }
 
 bool validateUser(const string& user, const string& pass) {
-    for (const auto& u : users) {
+    for (const auto& u : users)// for loop to ensure member is real  {
         if (u.getUsername() == user && u.getPassword() == pass) {
             return true; // valid user
         }
@@ -74,7 +77,7 @@ bool validateUser(const string& user, const string& pass) {
 }
 
 bool validateManager(const string& user, const string& pass) {
-    for (const auto& m : managers) {
+    for (const auto& m : managers) // for loop to ensure manager is real  {
         if (m.getUsername() == user && m.getPassword() == pass) {
             return true; // valid manager
         }
@@ -82,60 +85,59 @@ bool validateManager(const string& user, const string& pass) {
     return false; // invalid manager
 }
 
-void addUser(const string& user, const string& pass) {
+void addUser(const string& user, const string& pass) // function that will allow a new user to register{
     users.push_back(UserLogin(user, pass));
     cout << "User added successfully." << endl;
 }
-
-void addManager(const string& user, const string& pass) {
+void addManager(const string& user, const string& pass) // function that will allow a new manager to register{
     managers.push_back(ManagerLogin(user, pass));
     cout << "Manager added successfully." << endl;
 }
 };
 
-void loginPlatform(LoginManager& LoginManager) {
+void loginPlatform(LoginManager& LoginManager)// function that will let uer choose login or regster {
     string username, password;
     int choice, roleChoice;
 
     cout << "Welcome to the scorecard login platform!"<< endl;
     cout << "Please select your login option 1. Member Login\n2. Manager Login\n3.  Register New User\n4. Register New Manager\n";
-    cin >> choice;
+    cin >> choice; // initial user prompt to choose login or register
 
-    if (choice == 1) {
+    if (choice == 1)// member login instance  {
         cout << "Enter Username: ";
         cin >> username;
         cout << "Enter password: ";
         cin >> password;
 
-        if (LoginManager.validateUser(username, password)) {
+        if (LoginManager.validateUser(username, password))// if statement to validate user exists  {
             cout << "User login successful!" << endl;
         } else {
             cout << "Invalid username or password." << endl;
         }
-    } else if (choice == 2) { 
+    } else if (choice == 2)// manaager login instanve { 
 
         cout << "Enter username:";
         cin >> username;
         cout << "Enter password:";
         cin >> password;
 
-        if (LoginManager.validateManager(username, password)) {
+        if (LoginManager.validateManager(username, password))// if statement to validate manager exists  {
             cout << "Manager login successful!" << endl;
         } else {
             cout << "Invalid username or password." << endl;
         }
-    } else if (choice == 3) {
+    } else if (choice == 3)// regisration option for new members {
         cout << "Enter new username: ";
         cin >> username;
         cout << "Enter new password: ";
         cin >> password;
-        LoginManager.addUser(username, password);
-    } else if (choice == 4) {
+        LoginManager.addUser(username, password);// calling on new user function to add new user to system
+    } else if (choice == 4)// registration option for new managers {
         cout << "Enter new manager username: ";
         cin >> username;
         cout << "Enter new manager password: ";
         cin >> password;
-        LoginManager.addManager(username, password);
+        LoginManager.addManager(username, password);// calling on new manager function to add new manager to system
     } else {
         cout << "Invalid choice." << endl;
     }
@@ -144,5 +146,5 @@ void loginPlatform(LoginManager& LoginManager) {
 int main() {
     LoginManager loginManager;
     loginPlatform(loginManager);
-    return 0;
+    return 0;// main function to run login platform
 }
